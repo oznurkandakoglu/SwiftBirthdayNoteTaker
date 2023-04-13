ba95720c6ffece9462d172f9c7d071e4580605f5
Öznur Kandakoğlu <oznurkandakoglu@gmail.com>
2023-04-13 13:38:00 +0300

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Tiga  on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
